87d553897b3c9f019f0ac24f4593b3ae2cc48b1d
Gokula Krishnan <gokulakrishnan1996@gmail.com>
2023-01-27 14:48:46 +0000

    Finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -58,5 +58,11 @@ class PostDetailSerializer(PostSerializer):
 
         return instance
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+
 
 
