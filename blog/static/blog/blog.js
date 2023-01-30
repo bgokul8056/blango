a59d0f462a7d25ef9a6052145727880825530e23
Gokula Krishnan <gokulakrishnan1996@gmail.com>
2023-01-30 20:17:21 +0000

    Finish ReactJS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,24 +1,37 @@
-function resolvedCallback(data){
-  console.log('Resolved with data' + data);
-}
-
-function rejectCallback(msg){
-  console.log('Reject with message ' + msg);
-}
-
-const lazyAdd = (a,b) => {
-  const doAdd = (resolve, reject) => {
-    if (typeof a!=="number" || typeof b!=="number"){
-      reject("a and b must both be numbers")
-    }
-    else{
-      const sum = a+b
-      return resolve(sum)
-    }
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
   }
-  return new Promise(doAdd)
-}
 
-const p = lazyAdd(45,5)
-p.then(resolvedCallback, rejectCallback)
-lazyAdd("56","name").then(resolvedCallback, rejectCallback)
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if (this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        }
+      },
+      buttonText
+    )
+  }
+}
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
+
